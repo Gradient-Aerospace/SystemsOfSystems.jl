32abd8331c32851ca7bc70082d615e8894698e32
Tucker McClure <tucker@gradient.team>
2026-07-20 11:47:16 -0700

    Fix benchmark assertion

--- benchmark/speed_and_allocations.jl
+++ benchmark/speed_and_allocations.jl
@@ -577,14 +577,14 @@ function run_simulation(system_specs, solver, log, t_end)
     GC.gc()
     loop_outputs = SystemsOfSystems.loop!(inputs, runtime)
     result = SystemsOfSystems.tear_down(inputs, runtime, loop_outputs)
-    @assert result.history.stop isa ReachedEndTime
+    @assert result.history.stop isa SystemsOfSystems.ReachedEndTime
 
     inputs = make_inputs(system_specs, solver, log, t_end)
     runtime = SystemsOfSystems.make_runtime(inputs)
     GC.gc()
     @time loop_outputs = SystemsOfSystems.loop!(inputs, runtime)
     result = SystemsOfSystems.tear_down(inputs, runtime, loop_outputs)
-    @assert result.history.stop isa ReachedEndTime
+    @assert result.history.stop isa SystemsOfSystems.ReachedEndTime
 
     return (result.history, result.t_final, result.final_model)
 
