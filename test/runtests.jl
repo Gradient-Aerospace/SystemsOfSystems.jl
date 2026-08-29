4937814dd652eebb8111d656a74292ef2adf118e
Tucker McClure <tucker@gradient.team>
2026-08-28 21:32:16 -0700

    Automatically include test_*.jl files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,9 @@
 module RunTests
 
-# Start with core utilities and basic simulation behavior. The later tests exercise
-# additional simulation features and options that build on that behavior.
-include("test_step_triggering.jl")
-include("test_simulation_times.jl")
-include("test_schedules.jl")
-include("test_logging_policies.jl")
-include("test_time_series.jl")
-include("test_basic_simulations.jl")
-include("test_complex_states.jl")
-include("test_solver_options.jl")
-include("test_solver_lifecycle.jl")
-include("test_continuous_random_variables.jl")
-include("test_random_variable_seeds.jl")
-include("test_random_variable_draw_traversal.jl")
-include("test_control_system_demo.jl")
-include("test_updating_continuous_states.jl")
-include("test_resources.jl")
-include("test_hooks.jl")
-include("test_clock_sync.jl")
-include("test_sim_timeout.jl")
+for filename in sort(readdir(@__DIR__))
+    if startswith(filename, "test_") && endswith(filename, ".jl")
+        include(filename)
+    end
+end
 
 end # RunTests
